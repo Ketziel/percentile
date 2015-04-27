732fb08046f916cfc5c06a898179f62429c7bf42
Richard Wills <richardwills90@gmail.com>
2015-04-27 11:18:27 +0100

    base functionality

---
+++ jquery.percentile.js
@@ -0,0 +1,175 @@
+//Percentile
+function percentileVisibleCount(obj){
+	var screenWidth = $(window).width();
+	for (var property in obj) {
+		if (obj.hasOwnProperty(property)) {
+			if(property == "full" || parseInt(property) >= screenWidth){
+				return obj[property];
+			}
+		}
+	}
+}
+
+function percentileSlideWidth(slideCount, displayCount){
+	if (slideCount < displayCount){
+		return 100/slideCount;
+	} else {
+		return 100/displayCount;
+	}
+}
+
+function percentileGetWidthPercent(e){
+	return ( 100 * parseFloat(e.css('width')) / parseFloat(e.parent().css('width')) ) + '%';
+}
+
+function percentileGetWidthPercentInt(e){
+	//e.style.width causes undefined error... Not sure why!
+	return parseInt(( 100 * parseFloat(e.css('width')) / parseFloat(e.parent().css('width'))));
+}
+
+
+function percentileHeightWidth(elements, options){
+	return elements.each(function() {
+		var slideWidth = percentileSlideWidth($(this).find(options.slides).length, percentileVisibleCount(jQuery.parseJSON(options.breakpoints)));
+		$(this).find(options.slides).css('width', slideWidth+'%');
+		$(this).css('height',$(this).find(options.slides).first().height());
+	});
+}
+
+function percentileCreateRotate(elements, options, slideCount, displayCount){
+	return elements.each(function() {
+		if (slideCount > displayCount) {
+			$(options.next+', '+options.prev).addClass('active');
+			var slideWidth = percentileSlideWidth(slideCount, displayCount);	
+			
+			var i = 0;
+			$(this).find(options.slides).each(function(){
+				$(this).css('position', 'absolute');
+				$(this).css('left', (slideWidth*i)+"%");
+				i++;
+			});
+			
+			
+		} else {
+			$(options.next+', '+options.prev).removeClass('active');
+		}
+	});
+}
+
+(function( $ ){
+	$.fn.percentile = function(options) {
+
+		var defaults = {
+			slides: '.slide',
+			breakpoints:  '{ "full": 3, "768": 2, "520": 1 }',
+			next: '.slides .next',
+			prev: '.slides .prev'
+		}
+		var options =  $.extend(defaults, options);
+		var targets = this;
+		var animating = false;
+		
+		percentileHeightWidth(targets, options);
+		
+		//$(options.next, options.prev).fade();
+		
+		$(this).find(options.slides).on(
+			"transitionend MSTransitionEnd webkitTransitionEnd oTransitionEnd",
+			function() {
+				animating = false;
+				$(this).removeClass("animation");
+				
+				/*var slideWidthPercent = parseInt(this.style.width.replace('%',''));
+				var lastSlideLeft = 0;						
+				$(this).parent().find(options.slides).each(function() {
+					if (parseInt(this.style.left.replace('%',''))>lastSlideLeft){
+						lastSlideLeft = parseInt(this.style.left.replace('%',''));
+						console.log(lastSlideLeft);
+					}
+				});
+				
+				if (parseInt(this.style.left.replace('%','')) < 0){				
+					$(this).css('left',(lastSlideLeft + slideWidthPercent)+'%');				
+				} else if (parseInt(this.style.left.replace('%','')) > (lastSlideLeft)){
+					$(this).css('left',(slideWidthPercent-(slideWidthPercent*2))+'%');				
+				
+				}*/
+				
+			}
+		);
+		
+		function progressSlides(targets, direction){
+			if (animating == false) {
+				targets.each(function() {
+					var tar = $(this);
+					tar.find(options.slides).each(function(){
+						var slideWidthPercent = parseInt(this.style.width.replace('%',''));
+						var slide = this;
+						
+						
+						if (direction == "next" && parseInt(this.style.left.replace('%','')) < 0){			
+							$(this).css('left',(lastSlideLeft)+'%');	
+						} else if (direction != "next" && parseInt(this.style.left.replace('%','')) >= (lastSlideLeft)){
+							$(this).css('left',(slideWidthPercent-(slideWidthPercent*2))+'%');				
+						}
+						
+						setTimeout(function(){
+							
+							var offset = slideWidthPercent;
+							if (direction == "next"){
+								offset -= (offset*2);
+							}
+							offset += parseInt(slide.style.left.replace('%',''));
+							animating = true;
+							$(slide).addClass('animation');
+							$(slide).css('left', offset+'%');
+							$(slide).css('z-index', '0');
+							
+						
+						}, 100);
+						
+						
+					});
+				});
+			}
+		}
+		
+		$(options.next).click(function(e){
+			e.preventDefault();
+			progressSlides(targets, 'next');
+		});
+		
+		$(options.prev).click(function(e){
+			e.preventDefault();
+			progressSlides(targets, 'prev');
+		});
+		
+		percentileCreateRotate(targets, options, targets.find(options.slides).length, percentileVisibleCount(jQuery.parseJSON(options.breakpoints)));
+		
+		var lastSlideLeft = 0;						
+		targets.find(options.slides).each(function() {
+			if (parseInt(this.style.left.replace('%',''))>lastSlideLeft){
+				lastSlideLeft = parseInt(this.style.left.replace('%',''));
+				console.log(lastSlideLeft);
+			}
+		});
+		
+		/*return this.each(function() {
+
+		});*/
+		
+		
+		
+		$(window).bind('resize',$.proxy(function(){
+			targets.find(options.slides).each(function() {
+				if (parseInt(this.style.left.replace('%',''))>lastSlideLeft){
+					lastSlideLeft = parseInt(this.style.left.replace('%',''));
+					console.log(lastSlideLeft);
+				}
+			});
+			percentileHeightWidth(targets, options);
+			percentileCreateRotate(targets, options, $(this).find(options.slides).length, percentileVisibleCount(jQuery.parseJSON(options.breakpoints)));
+		}, this));
+		
+	};
+})( jQuery );
